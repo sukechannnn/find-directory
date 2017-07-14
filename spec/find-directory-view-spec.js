7776f254b06111323c632cd0f3595af59371ad63
yosuke0315 <wonderfulworld0315@gmail.com>
2017-07-14 22:07:26 +0900

    Atom generate package

---
+++ spec/find-directory-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FindDirectoryView from '../lib/find-directory-view';
+
+describe('FindDirectoryView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
